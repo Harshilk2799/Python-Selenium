64fd255262eafcadeea2ae4b9ba6db1014148c22
Harshil Khatri <khatriharshil26@gmail.com>
2026-04-20 14:25:36 +0530

    Implementation navigation of web page in selenium.

--- 1.Selenium_Navigate_Link/selenium_1.py
+++ 1.Selenium_Navigate_Link/selenium_1.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 
-
-service_obj = Service("/home/harshil/Selenium/Drivers/chromedriver_linux64/chromedriver")
-
-driver = webdriver.Chrome(service=service_obj)
+driver = webdriver.Chrome()
 
 # Navigating links using get method
 
 # get() = Opening the application URL 
 driver.get("https://opensource-demo.orangehrmlive.com/")
+print(driver.title)
 driver.close()
