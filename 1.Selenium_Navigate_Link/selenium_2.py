a0f3876ea3d0d0978654d8ee7dff6d7147027080
Harshil Khatri <khatriharshil26@gmail.com>
2026-04-20 14:26:15 +0530

    Implementation of webdriver manager in selenium.

---
+++ 1.Selenium_Navigate_Link/selenium_2.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# Navigating links using get method
+
+# get() = Opening the application URL 
+driver.get("https://opensource-demo.orangehrmlive.com/")
+print(driver.title)
+driver.close()
