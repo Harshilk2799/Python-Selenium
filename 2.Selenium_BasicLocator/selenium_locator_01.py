250a9080a685ac7a77f73a25935a8972a4e4299a
Harshil Khatri <khatriharshil26@gmail.com>
2026-04-21 10:54:20 +0530

    Implementation of Basic Locator(ID, Name, Link Text and Partial Link) in selenium.

--- 2.Selenium_BasicLocator/selenium_locator_01.py
+++ 2.Selenium_BasicLocator/selenium_locator_01.py
@@ -3,9 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-service_obj = Service("/home/harshil/Selenium/Drivers/chromedriver_linux64/chromedriver")
-
-driver = webdriver.Chrome(service=service_obj)
+driver = webdriver.Chrome()
 
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()    # maximize window
