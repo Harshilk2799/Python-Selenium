9b333fa7d14256d7ed7f05e4099ba2cbed773191
Harshil Khatri <khatriharshil26@gmail.com>
2026-04-23 10:15:24 +0530

    Implementation of navigation commands in selenium.

--- 4.Selenium_commands/Navigationalcommands_04.py
+++ 4.Selenium_commands/Navigationalcommands_04.py
@@ -1,10 +1,7 @@
 from selenium import webdriver 
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service_obj = Service(executable_path="/home/harshil/Selenium/Drivers/chromedriver_linux64/chromedriver")
-
-driver = webdriver.Chrome(service = service_obj)
+driver = webdriver.Chrome()
 
 driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F")
 driver.get("https://www.amazon.com")
