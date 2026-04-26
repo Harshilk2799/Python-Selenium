39e1a0df5d9ba9ca7de51959314611b228e66068
Harshil Khatri <khatriharshil26@gmail.com>
2026-04-26 10:31:42 +0530

    Implementation of handle broken link in selenium.

--- 7.Selenium_link/handlebroken_link.py
+++ 7.Selenium_link/handlebroken_link.py
@@ -1,11 +1,8 @@
 from selenium import webdriver 
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import requests
 
-service_obj = Service(executable_path="/home/harshil/Selenium/Drivers/chromedriver_linux64/chromedriver")
-
-driver = webdriver.Chrome(service = service_obj)
+driver = webdriver.Chrome()
 
 driver.get("http://www.deadlinkcity.com/")
 driver.maximize_window()
