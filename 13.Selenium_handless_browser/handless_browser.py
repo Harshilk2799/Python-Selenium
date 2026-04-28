4c39f7e97b566a0fc8eec78876ceac75d26bd629
Harshil Khatri <khatriharshil26@gmail.com>
2026-04-28 11:03:21 +0530

    Implementation of headless browser in selenium.

--- 13.Selenium_handless_browser/handless_browser.py
+++ 13.Selenium_handless_browser/handless_browser.py
@@ -1,6 +1,4 @@
-from curses import window
 from selenium import webdriver 
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
@@ -8,9 +6,8 @@ option = Options()
 option.headless = True
 
 # option.add_argument("--incognito")
-service_obj = Service(executable_path="/home/harshil/Selenium/Drivers/chromedriver_linux64/chromedriver")
 
-driver = webdriver.Chrome(service = service_obj, options=option)
+driver = webdriver.Chrome(options=option)
 
 driver.get("https://whatmylocation.com/")
 driver.maximize_window()
