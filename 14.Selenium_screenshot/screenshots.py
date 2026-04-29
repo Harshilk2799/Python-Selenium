966828d1003b1e16dd16283e2d26fcf1e5e501d8
Harshil Khatri <khatriharshil26@gmail.com>
2026-04-29 13:25:51 +0530

    Implementation of screenshot of web page in selenium.

--- 14.Selenium_screenshot/screenshots.py
+++ 14.Selenium_screenshot/screenshots.py
@@ -1,13 +1,6 @@
-from curses import window
 from selenium import webdriver 
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
-
-service_obj = Service(executable_path="/home/harshil/Selenium/Drivers/chromedriver_linux64/chromedriver")
-
-driver = webdriver.Chrome(service = service_obj)
+driver = webdriver.Chrome()
 
 driver.get("https://whatmylocation.com/")
 driver.maximize_window()
